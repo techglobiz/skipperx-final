b7c7c69c0c0c3921c58f865f73981dcf23f71177
Abbas Haider <developers@techglobiz.com>
2025-08-08 16:02:30 +0530

    added coming soon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+
+import "./coming-soon.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -41,11 +41,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Coming Soon Banner for all pages */}
+         <div className="coming-soon-message">
+      <p>
+        SkipperX is cooking something the internet isn&apos;t ready for.
+      </p>
+      <h3 className="coming-soon-subheading">We&apos;re not late, We&apos;re just loading greatness.</h3>
+    </div>
+        {/*
         <Header />
         <main className="min-h-screen">
           {children}
         </main>
         <Footer />
+        */}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,42 @@
-// ...existing code...
-import BackedByBest from "../components/BackedByBest";
-import CareerFormSection from "../components/CareerFormSection";
-import Circle from "../components/Circle";
-import EngineeringPrograms from "../components/EngineeringPrograms";
-import Hero from "../components/Hero";
-import LaunchSection from "../components/LaunchSection";
-import SkippersAdvantage from "../components/SkippersAdvantage";
-import SuperStack from "../components/SuperStack";
-import TestimonialCarousel from "../components/TestimonialCarousel";
-import TrustSection from "../components/TrustSection";
-import WhyTrustUs from "../components/WhyTrustUs";
+// --- Original Home Page code below ---
+// import BackedByBest from "../components/BackedByBest";
+// import CareerFormSection from "../components/CareerFormSection";
+// import Circle from "../components/Circle";
+// import EngineeringPrograms from "../components/EngineeringPrograms";
+// import Hero from "../components/Hero";
+// import LaunchSection from "../components/LaunchSection";
+// import SkippersAdvantage from "../components/SkippersAdvantage";
+// import SuperStack from "../components/SuperStack";
+// import TestimonialCarousel from "../components/TestimonialCarousel";
+// import TrustSection from "../components/TrustSection";
+// import WhyTrustUs from "../components/WhyTrustUs";
+
+import "./coming-soon.css";
 
 export default function Home() {
+  // --- Original return below ---
+  // return (
+  //   <>
+  //     <Hero />
+  //     <TrustSection />
+  //     <TestimonialCarousel />
+  //     <EngineeringPrograms />  
+  //     <SuperStack />
+  //     <CareerFormSection />
+  //     <SkippersAdvantage />
+  //     <Circle />
+  //     <LaunchSection />
+  //     <BackedByBest />
+  //     <WhyTrustUs />
+  //   </>
+  // );
   return (
-   <>
-
-
-      <Hero />
-      <TrustSection />
-      <TestimonialCarousel />
-      <EngineeringPrograms />  
-      <SuperStack />
-      <CareerFormSection />
-      <SkippersAdvantage />
-      <Circle />
-      <LaunchSection />
-      <BackedByBest />
-      <WhyTrustUs />
-      
-
-    </>
+    <div className="coming-soon-message">
+      <p>
+        SkipperX is cooking something the internet isn&apos;t ready for.
+      </p>
+      <h3 className="coming-soon-subheading">We&apos;re not late, We&apos;re just loading greatness.</h3>
+    </div>
   );
 }
+
